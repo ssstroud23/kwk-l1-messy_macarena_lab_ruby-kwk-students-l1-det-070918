61282e40e7844ec72c54f9bb39f7dfe682745c5f
ssstroud23 <ssstroud23@gmail.com>
2018-07-10 17:42:38 +0000

    Done.

--- macarena.rb
+++ macarena.rb
@@ -8,23 +8,23 @@ puts ""
 # Step 1: GOOD
 puts "Step 1: Right arm out in front of you, palm facing down."
 
-# Step 2: FIXME
-puts "Step 2: Turn your right palm up."
+# Step 2: GOOD
+puts "Step 2: Left arm out in front of you, palm facing down."
 
-# Step 3: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
+# Step 3: GOOD
+puts "Step 3: Turn your right palm up."
 
-# Step 4: FIXME
+# Step 4: GOOD
 puts "Step 4: Turn your left palm up."
 
-# Step 5: FIXME
-puts "Step 5: Bring your right hand up and around to the back of your head."
+# Step 5: GOOD
+puts "Place your right hand on your left shoulder."
 
-# Step 6: FIXME
-puts "Step 6: Left arm out in front of you, palm facing down."
+# Step 6: GOOD
+puts "Place your left hand on your right shoulder."
 
-# Step 7: FIXME
-puts "Step 7: Place your left hand on your left shoulder."
+# Step 7: GOOD
+puts "Bring your right hand up and around to the back of your head."
 
 # Step 8: FIXME
-puts "Step 8: The Last Step"
+puts "Bring your left hand up and around to the back your head."
